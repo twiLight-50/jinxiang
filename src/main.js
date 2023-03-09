f46d95fcbd8ca062f9caaf1a3c97d8722f137535
twiLight-50 <17333142362@163.com>
2023-03-09 18:03:29 +0800

    提交所有数据

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+// 导入自定义UI组件库
+import UI from '@/components/library'
+// 导入重置样式库
+import 'normalize.css'
+// 导入自己的重置样式库
+import '@/assets/styles/common.less'
+createApp(App).use(store).use(ElementPlus).use(router).use(UI).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,44 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig,path } = require("@vue/cli-service",'path');
+
+const path = require("path");
+
+module.exports = ({
+  transpileDependencies: true,
+  lintOnSave: false,
+
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      // 那些文件需要引入，使用绝对路径
+      // 需要是path.join来拼接
+      patterns: [
+        path.join(__dirname, './src/assets/styles/mixins.less'),
+        path.join(__dirname, "./src/assets/styles/variables.less"),
+      ],
+    },
+  },
+  devServer: {
+    port: 8080,
+    host: '127.0.0.1',
+    https: false,
+    open: true,
+    allowedHosts: 'all'
+  },
+  // chainWebpack: config => {
+    // 图片加载
+    // config.module
+    //   .rule('images')
+    //   .use('url-loader')
+    //   .loader('url-loader')
+    //   .tap(options => Object.assign(options, { limit: 10000 }))
+
+    // 开启IP域名访问
+    // config.devServer.disableHostCheck(true)
+  // },
+  
+  configureWebpack: {
+    externals: {
+      qc: 'QC'
+    }
+  },
+});
